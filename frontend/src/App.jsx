49438a8103fd1fa8ffc17006e0f6b29f57ee9878
Max O'Meara <mxomeara@bu.edu>
2025-01-09 19:04:05 -0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <p>hello</p>
+       <Excuse/>
+       <Reasoning/>
+      </div>
+    </>
+  )
+}
+
+function Excuse(){
+  return (
+    <>
+    <p>I will work on this later</p>
+    </>
+  )
+}
+
+function Reasoning(){
+  return(
+    <>
+    <p>I have to leave work and make dinner</p>
+    </>
+  )
+}
+
+
+
+export default App
