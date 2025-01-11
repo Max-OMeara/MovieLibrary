c5020ffedf648db59adda77604fd8730bee25493
Max O'Meara <mxomeara@bu.edu>
2025-01-11 12:18:34 -0500

    Oops

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "0b82a13b04dc8daa972767d6c4f17cbc";
+const API_KEY = "";
 const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () => {
@@ -8,7 +8,7 @@ export const getPopularMovies = async () => {
 };
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie/?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
     const data = await response.json()
     return data.results
 };
